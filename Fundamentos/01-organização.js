62bb0ed1511553eca2e339661fff0456d31d747f
Thiago Aquino <aquinolima.thiago@gmail.com>
2020-10-13 18:10:47 -0300

    Fundamentos - Organização

---
+++ Fundamentos/01-organização.js
@@ -0,0 +1,30 @@
+// Organização Básica
+
+    console.log("Setença de código");
+
+    {
+        //Bloco de código, tem a função de juntar comandos de código.
+        console.log("Olá");
+        console.log("Mundo!") //Não há necessidade de fechar o código com o ";"
+    }
+
+// Executando o javascript
+
+    // sites que é possivel usar o js        
+        console.log("https://repl.it/");
+        console.log("https://jsfiddle.net/");
+
+// Comentários
+
+    console.log("tipos de comentários"); 
+        // comentário de uma linha
+
+        /*
+            comentário em sentença de código.
+        */
+
+        /*
+            * comentário com, 
+            * identificação de 
+            * linhas
+        */
