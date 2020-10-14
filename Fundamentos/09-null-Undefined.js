13ba1b8fe2240d2601d568f9c305330f14aff66e
Thiago Aquino <aquinolima.thiago@gmail.com>
2020-10-14 17:24:45 -0300

    Fundamentos - Conceitos de null e Undefined

---
+++ Fundamentos/09-null-Undefined.js
@@ -0,0 +1,8 @@
+let valor // não será inicializada
+console.log(valor);  // aqui temos o valor undefined, pois o valor foi inicializado.
+
+valor = null ; //null significada que a variável não está apontando para nenhum endereço de memória.
+// ela não tem um valor dentro dela.
+// null = ausência de valor
+console.log(valor); //diferente do undefined, a variável foi inicializada, 
+//porém não aponta pra nenhum lugar de memória.
